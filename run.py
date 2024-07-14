f44a60a515e4cc772a7e640802c62e9f2b1edda2
Blake Collins <blakecollins78@gmail.com>
2024-07-14 12:17:27 +0000

    ADD: adding and updating contact form

--- run.py
+++ run.py
@@ -12,12 +12,22 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html" , page_title= "About")
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html" , page_title= "Contact")
+
+
+@app.route("/services")
+def services():
+    return render_template("services.html" , page_title= "Services")
+
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html" , page_title= "Portfolio")
 
 
 
